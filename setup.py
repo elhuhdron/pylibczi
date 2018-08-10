2daba7f170540ac5910714ebd72d8df8876285c6
Paul Watkins <pwatkins@gmail.com>
2018-08-11 00:29:14 +0200

    static libCZI linking

--- setup.py
+++ setup.py
@@ -6,10 +6,6 @@ import sysconfig
 import platform
 import subprocess
 
-# libczi cloned as submodule
-include_libCZI = os.path.join('.', 'libCZI', 'Src')
-lib_libCZI = os.path.join('.', 'libCZI', 'build', 'Src', 'libCZI')
-
 
 def build_libCZI():
     env = os.environ.copy()
@@ -34,25 +30,50 @@ if platform_ == 'Linux':
     #os.environ["CC"] = "g++-6"; os.environ["CXX"] = "g++-6"
     extra_compile_args += ["-std=c++11", "-Wall", "-O3"]
 elif platform_ == 'Darwin':
-    extra_compile_args += ["-std=c++11", "-Wall", "-O3", "-stdlib=libc++", "-mmacosx-version-min=10.9"]
+    mac_ver = platform.mac_ver()[0]
+    extra_compile_args += ["-std=c++11", "-Wall", "-O3", "-stdlib=libc++", "-mmacosx-version-min="+mac_ver]
 elif platform_ == 'Windows':
     assert(False) # xxx - not tested on windows
 
 extra_link_args = sysconfig.get_config_var('LDFLAGS').split()
 extra_link_args += extra_compile_args
 
+# second answer at
+# https://stackoverflow.com/questions/4597228/how-to-statically-link-a-library-when-compiling-a-python-module-extension
+
+# libczi cloned as submodule
+include_libCZI = os.path.join('.', 'libCZI', 'Src')
+lib_libCZI = os.path.join('.', 'libCZI', 'build', 'Src', 'libCZI')
+lib_JxrDecode = os.path.join('.', 'libCZI', 'build', 'Src', 'JxrDecode')
+
+static_libraries = ['libCZIStatic', 'JxrDecodeStatic']
+static_lib_dirs = [lib_libCZI, lib_JxrDecode]
+libraries = []
+library_dirs = []
+include_dirs = [numpy.get_include(), include_libCZI]
+
+if platform_ == 'Windows':
+    libraries.extend(static_libraries)
+    library_dirs.extend(static_lib_dirs)
+    extra_objects = []
+else: # POSIX
+    extra_objects = ['{}/lib{}.a'.format(d, l) for d,l in zip(static_lib_dirs, static_libraries)]
+    include_dirs.append('/usr/local/include')
+    library_dirs.append('/usr/local/lib')
+
 sources = ['_pylibczi.cpp']
 
 module1 = Extension('_pylibczi',
                     define_macros = [('MAJOR_VERSION', '0'),
                                      ('MINOR_VERSION', '1')],
-                    include_dirs = ['/usr/local/include', numpy.get_include(), include_libCZI],
-                    libraries = ['libCZI'],
-                    library_dirs = ['/usr/local/lib', lib_libCZI],
+                    include_dirs = include_dirs,
+                    libraries = libraries,
+                    library_dirs = library_dirs,
                     sources = [os.path.join('_pylibczi',x) for x in sources],
                     extra_compile_args=extra_compile_args,
                     extra_link_args=extra_link_args,
                     language='c++11',
+                    extra_objects=extra_objects,
                     )
 
 setup (name = 'pylibczi',
