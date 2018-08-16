5e89cd918fb383dce1c40a48b7b2371f938bc92a
Paul Watkins <pwatkins@gmail.com>
2018-08-16 13:20:41 +0200

    going back to libCZI fork

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,12 +5,16 @@ set -e -x
 yum install -y libpng-devel zlib-devel
 
 # Install newer devtools that work on manylinux
-GCCLOC=https://github.com/squeaky-pl/centos-devtools/releases/download/6.3
-GCCAR=gcc-6.3.0-binutils-2.27-x86_64.tar.bz2
-curl -o ${GCCAR} -L ${GCCLOC}/${GCCAR}
-tar xvjf ${GCCAR}
+#GCCLOC=https://github.com/squeaky-pl/centos-devtools/releases/download/6.3
+#GCCAR=gcc-6.3.0-binutils-2.27-x86_64.tar.bz2
+#export CC=/opt/devtools-6.3/bin/gcc
+#export CXX=/opt/devtools-6.3/bin/g++
+GCCLOC=https://github.com/Noctem/pogeo-toolchain/releases/download/v1.5
+GCCAR=gcc-7.3-centos5-x86-64.tar.bz2
 export CC=/toolchain/bin/gcc
 export CXX=/toolchain/bin/g++
+curl -o ${GCCAR} -L ${GCCLOC}/${GCCAR}
+tar xvjf ${GCCAR}
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
